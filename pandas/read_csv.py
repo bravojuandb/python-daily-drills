5fa9179bba3672091f24ff0d03ad2c8170fa7bf8
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-04 08:01:04 +0200

    add basic reading, fixed errors UnicodeDecodeError, and ParseError

--- pandas/read_csv.py
+++ pandas/read_csv.py
@@ -3,4 +3,24 @@ Short drill on reading a CSV file using Pandas.
 
 Related to my supplier data pipelina project
 
+
+
+COMMON ERRORS:
+
+UnicodeDecodeError  fails to read the file. Satandard decoding utf-8
+pandas.errors.ParserError  fails because of the delimiter. standard: ","
+
+
 """
+
+
+
+
+print(">>> Script started")
+
+import pandas as pd
+
+df = pd.read_csv("sample.csv", encoding= "latin1", delimiter=";")
+
+print(">>> DataFrame loaded")
+
