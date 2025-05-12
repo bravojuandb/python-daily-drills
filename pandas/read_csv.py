6b2036248391f332c3da2b07d625b3e4406b0350
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-12 06:56:27 +0200

    feat: finish basic structure of the script: read csv, manage patmake it relative to the script´s location

--- pandas/read_csv.py
+++ pandas/read_csv.py
@@ -1,26 +1,34 @@
 """
-Short drill on reading a CSV file using Pandas.
+Short drill on reading a CSV file using Pandas. 
 
-Related to my supplier data pipelina project
+This is Version 1 of a basic script that demonstrates:
+- How to read a CSV file using Pandas
+- How to use `pathlib` to manage file paths
+- How to make the script runnable from any directory using `__file__`
 
+This script is part of my supplier data pipeline project
 
+COMMON ERRORS ENCOUNTERED:
 
-COMMON ERRORS:
-
-UnicodeDecodeError  fails to read the file. Satandard decoding utf-8
-pandas.errors.ParserError  fails because of the delimiter. standard: ","
-
+    UnicodeDecodeError  fails to read the file. Satandard decoding utf-8
+    pandas.errors.ParserError  fails because of the delimiter. standard: ","
 
 """
+from pathlib import Path
+import pandas as pd
 
-
-
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "sample.csv"
+OUTPUT_DIR = BASE_DIR / "output.csv"
 
 print(">>> Script started")
 
-import pandas as pd
-
-df = pd.read_csv("sample.csv", encoding= "latin1", delimiter=";")
+df = pd.read_csv(DATA_DIR, encoding= "latin1", delimiter=";")
 
 print(">>> DataFrame loaded")
 
+print(f"\n This is a sample of the data frame:\n {df}")
+
+print(f"\nThese are the column names of the data frame: \n {df.columns}")
+
+print(f"\nColumn types and non-null counts:\n {df.info()}")
