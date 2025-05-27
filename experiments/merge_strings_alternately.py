931a8dc057bdd1de2e3e0c5a60583230f8658df9
Juan David Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-27 10:22:02 +0000

    feat: add first version to practice what a method is and how to call it.

--- experiments/merge_strings_alternately.py
+++ experiments/merge_strings_alternately.py
@@ -4,3 +4,16 @@ Leetcode string merging problem.
 Features:
 
 """
+# Let´s start creating a method to say Hello
+
+class Greeter:
+    def say_hello (self):
+        print("Hello Socrates!")
+
+# Create an instance of the class
+
+g = Greeter
+
+# Call the method
+
+g.say_hello()
