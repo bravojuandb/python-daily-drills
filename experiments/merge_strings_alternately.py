840852fd38a25e244244e065680dd7146f7160b9
Juan David Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-27 11:01:43 +0000

    feat: add strings_merger function to merge_strings_alternately.py

--- experiments/merge_strings_alternately.py
+++ experiments/merge_strings_alternately.py
@@ -3,23 +3,22 @@ Leetcode string merging problem.
 
 Features:
 
-Plain script version
+Wrapping the simple script into a function
 """
-# Define the two words
 
-word1 = "abc"
-word2 = "pqrs"
 
-result = []
+def strings_merger (word1, word2):
+    
+    result = []
 
-max_length = max(len(word1), len(word2))
+    max_length = max(len(word1), len(word2))
 
-for i in range(max_length):
-    if i < len(word1):
-        result.append(word1[i])
-    if i < len(word2):
-        result.append(word2[i])
+    for i in range(max_length):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
 
-merged = ''.join(result)
+    return ''.join(result)
 
-print(merged)
+print(strings_merger("socrates", "aristoteles"))
