6e49b2678f6d5a178e5f007f2241152e90712847
Juan David Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-27 11:15:20 +0000

    chore: add short comments on class and object roles

--- experiments/merge_strings_alternately.py
+++ experiments/merge_strings_alternately.py
@@ -6,19 +6,25 @@ Features:
 Wrapping the simple script into a function
 """
 
+class Solution:
 
-def strings_merger (word1, word2):
-    
-    result = []
+    def mergeAlternately (self, word1: str, word2: str) -> str:
+        
+        result = []
 
-    max_length = max(len(word1), len(word2))
+        max_length = max(len(word1), len(word2))
 
-    for i in range(max_length):
-        if i < len(word1):
-            result.append(word1[i])
-        if i < len(word2):
-            result.append(word2[i])
+        for i in range(max_length):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
 
-    return ''.join(result)
+        return ''.join(result)
 
-print(strings_merger("socrates", "aristoteles"))
+sol = Solution() # Create the sol object
+
+# Call the mergeAlternately method on that sol object.
+result = sol.mergeAlternately("euclides", "aristofanes") 
+
+print(result)
