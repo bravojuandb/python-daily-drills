5a1ff2381ad3d6d9bd12a2074ac218f231fb18f3
Juan David Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-05-27 15:57:59 +0000

    feat: refactor count_words.py using pathlib and wrap logic into function

--- experiments/count_words.py
+++ experiments/count_words.py
@@ -1,26 +1,36 @@
 
 """
 Count the words of a .txt file
+Upgrade using Pathlib, wrapping the logic inside a function.
 
 """
 
-import os
+from pathlib import Path
 
-# Get the folder where the script itself is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
+def analyze_text_file(filename):
 
-# Build the full path to the file
-file_path = os.path.join(script_dir, 'carmina_I_11.txt')
+    # Get path of the current script
+    script_path = Path(__file__).resolve()
 
-# Open it
-with open(file_path, "r", encoding='utf-8') as file:
-    text = file.read()  # text variable is a string
+    # Get the folder where the script lives
+    script_dir = script_path.parent
 
-words = text.split()
-word_count = len(words)
+    # Build the full path to the text file
+    file_path = script_dir / filename
 
-print(f"\n This is the list: {words}")
-print(f"\n Number of words: {word_count}")
-print(f"\n This is the folder where the script is: {script_dir}")
-print(f"\n This is the path to the python script: {__file__}")
-print(type(words))
+    # Read the file
+    text = file_path.read_text(encoding='utf-8')
+
+    # Process the text
+    words = text.split()
+    word_count = len(words)
+
+    print(f"\n The text converted to list: {words}")
+    print(f"\n Number of words: {word_count}")
+    print(f"\n This is the folder where the script lives: {script_dir}")
+    print(f"\n This is the path to the python script: {__file__}")
+    print(type(words))
+
+    return words, word_count
+
+words, count = analyze_text_file("carmina_I_11.txt")
