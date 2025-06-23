cf2d0f5e52229882fb9ea7860d166617a7af874e
Juan David Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-23 03:22:53 +0000

    chore: add new empty exercise first_unique_character.py, pillar 1, level easy

---
+++ level1_fluency/first_unique_character.py
@@ -0,0 +1,9 @@
+"""
+First Unique Character, difficulty: easy
+
+Write a function that receives a lowercase string and returns the index of
+the first non-repeating character. If there's no unique character, treturn -1.
+
+"""
+
+def 
