0021603af4150db8b5b40dad0d1167f796c3fe3a
Juan D. Bravo <196306536+juan-dbravo@users.noreply.github.com>
2025-06-24 10:32:52 +0000

    chore: add empty script simple_mixer.py with problestatement

---
+++ level1_fluency/simple_mixer.py
@@ -0,0 +1,18 @@
+"""
+Given two lowercase strings, a and b
+each containing space-separated words
+with no punctuations and no capital letters.
+
+Write a funtion that returns a sorted list 
+of unique words that appear in either string.
+
+- lowecase words
+- list of unique words, dont include set()
+- output sorted alfabetically
+- no external libraries
+
+"""
+
+def word_mixer (a: str, b:str) -> list:
+
+    
