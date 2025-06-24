7ce1c8f8eaaad674d4a16b803d388b14d1069040
Juan D. Bravo <196306536+juan-dbravo@users.noreply.github.com>
2025-06-24 10:45:47 +0000

    feat: add simple_mixer.py complete, easy version.

--- level1_fluency/simple_mixer.py
+++ level1_fluency/simple_mixer.py
@@ -14,5 +14,19 @@ of unique words that appear in either string.
 """
 
 def word_mixer (a: str, b:str) -> list:
+    words_a = a.split()
+    words_b =b.split()
+    all_words = words_a + words_b
+    unique_words = []
+    for word in all_words:
+        if word not in unique_words:
+            unique_words.append(word)
+    return sorted(unique_words)
 
-    
+a = "hello world this is a test"
+b = "this test is just a test hello world"
+
+result = word_mixer(a,b)
+
+print(result)
+
