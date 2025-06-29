ce8115e7164e66d3f8a96c71e72df0ab4e3d34e7
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-29 07:35:15 +0200

    feat: implement two functions to return the firs non repeating character of a string, second f is a simplifies version of the first one

--- level1_fluency/unique_char_med.py
+++ level1_fluency/unique_char_med.py
@@ -4,7 +4,34 @@ This is medium difficulty drill.
 Write a functions that returns 
 the first character in a string that does not repeat.
 
+•	Ignore spaces
+•	Be case-sensitive
+•	Use any built-in structures (like dict, collections.Counter, etc.)
+•	Aim for O(n) time if possible
+
 """
+from collections import Counter
 
 def first_unique_char(s: str) -> str | None:
-    pass
+    counts = Counter(s)
+    unique = []
+    for i, j in counts.items():
+        if j == 1:
+            unique.append(i)
+    for char in unique:
+        if char in s:
+            print("Found the first non repeating character!!!")
+        return char
+    return None
+
+
+def first_unique_char_improved (s: str) -> str | None:
+    counts = Counter(s)
+
+    for char in s:
+        if counts[char] == 1:
+            return char
+    return None
+
+
+print(first_unique_char_improved("es.tern oclei domastoideo"))
