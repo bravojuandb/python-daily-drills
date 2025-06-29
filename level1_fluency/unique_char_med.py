cadec9d53c38fb73227fc4bb00e09aca0c6d5dff
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-29 07:00:54 +0200

    chore: add medium problem from level1, with description and function signature

---
+++ level1_fluency/unique_char_med.py
@@ -0,0 +1,10 @@
+"""
+This is medium difficulty drill.
+
+Write a functions that returns 
+the first character in a string that does not repeat.
+
+"""
+
+def first_unique_char(s: str) -> str | None:
+    pass
