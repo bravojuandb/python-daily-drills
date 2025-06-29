a2732b2a825dde7cda81dc8b03785e6941bafa4e
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-29 08:05:30 +0200

    feat: add function to extract longest word from a sentence, stripping punctuation using string.punctuation

--- level1_fluency/longest_word_med.py
+++ level1_fluency/longest_word_med.py
@@ -7,6 +7,22 @@ Write a function that takes a sentence as input and returns the longest word in 
 If multiple words have the same length, return the first one.
 
 """
+import string
 
 def longest_word(sentence: str) -> str:
-    pass
+
+    words = sentence.split()
+    clean_words = []
+    for word in words:
+        cl_word = word.strip(string.punctuation)
+        clean_words.append(cl_word)
+
+    return max(clean_words, key=len)
+
+sentence = ("""La libertad, Sancho, es uno de los más preciosos dones 
+            que a los hombres dieron los cielos; con ella no pueden igualarse 
+            los tesoros que encierran la tierra ni el mar encubre; 
+            por la libertad, así como por la honra, 
+            se puede y debe aventurar la vida.""")
+
+print(longest_word(sentence))
