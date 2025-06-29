f705e6c4de594163f07651c9b7851a196b92e6de
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-29 07:40:57 +0200

    chore: add new medium level drill from pillar1, with description and function signature.

---
+++ level1_fluency/longest_word_med.py
@@ -0,0 +1,12 @@
+"""
+Longest Word in a Sentence
+
+Description:
+
+Write a function that takes a sentence as input and returns the longest word in it.
+If multiple words have the same length, return the first one.
+
+"""
+
+def longest_word(sentence: str) -> str:
+    pass
