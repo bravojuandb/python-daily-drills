b76944ef41e9aea995eeb02ef97b5c1c605882bf
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-30 05:42:42 +0200

    chore: add easy-medium problem on removing duplicates from a list, problem description and function signature

---
+++ level1_fluency/remove_duplicates2.py
@@ -0,0 +1,10 @@
+"""
+Write a function that removes duplicates from a list without changing the order of the elements.
+
+	•	Don't use set(nums) — it won't preserve order
+	•	Use a loop + a helper structure like a set or another list
+
+"""
+
+def remove_duplicates(nums: list[int]) -> list[int]:
+    pass
