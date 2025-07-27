8069a37758afa12be91caf3bd60c6b29a7a49f5b
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-07-27 06:24:04 +0200

    feat: add explanatory functions for drill_1.py

--- level1_fluency/group_by_lenght/drill_2.py
+++ level1_fluency/group_by_lenght/drill_2.py
@@ -15,7 +15,7 @@ def lowercase_unique_words (words: list[str]) -> set[str]:
 def lowercase_unique_words (words: list[str]) -> set[str]:
     return set(word.lower() for word in words)
 
-print(lowercase_unique_words(["Tree", "tree", "CAT", "Cat"]))
+lowercase_unique_words(["Tree", "tree", "CAT", "Cat"])
 
 
 # Block num 2: Group words by lenght
@@ -31,4 +31,22 @@ def group_by_length_basic(words: list[str]) -> dict[int, list[str]]:
         groups[lenght].append(word)
     return groups
 
-print(group_by_length_basic(["cat", "house", "tree"]))
+group_by_length_basic(["cat", "house", "tree"])
+
+# Block num: 3
+# Goal: Given a dictionary of sets, return a dictionary of sorted lists.
+
+def sort_grouped_words(groups: dict[int, set[str]]) -> dict[int, list[str]]:
+
+    sorted_group = {}
+    for length, words in groups.items():
+        sorted_group[length] = sorted(words)
+
+    return sorted_group
+
+#def sort_grouped_words(groups: dict[int, set[str]]) -> dict[int, list[str]]:
+#   return {length: sorted(list(words)) for length, words in sorted(groups.items())}
+
+
+print(sort_grouped_words({3: {'dog', 'cat'}, 5: {'zebra', 'apple'}}))
+# {3: ['cat', 'dog'], 5: ['apple', 'zebra']}
