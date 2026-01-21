6d45fb80559eee1dda131f2351e7d1b062b243e9
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2026-01-21 10:50:27 +0000

    chore(p3/8.1): add drill01 prompt

---
+++ pillar3/bash-orchestration/drill01_args.sh
@@ -0,0 +1,14 @@
+"""
+Bash execution & arguments
+
+Goal
+Understand how a Bash script receives input.
+
+Tasks
+- Print the script name and the values of `$1` and `$2`
+- Run it with two arguments: `2002-01 2002-03`
+
+Outcome
+- You can explain how `FROM` and `TO` are populated
+- You know how arguments reach a script
+"
