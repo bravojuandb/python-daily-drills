a42284b052eefdaea77f4418a38011a2597ecb82
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2026-07-28 06:51:49 +0200

    test(p2/core-processing-patterns): add unit tests for normalize_user

--- tests/test_p2_b_core_processing_patterns.py
+++ tests/test_p2_b_core_processing_patterns.py
@@ -58,3 +58,61 @@ def test_filter_active_does_not_mutate_input_records():
     filter_active(records)
 
     assert records == original
+
+
+# Test for b_normalilze_records.py
+
+from pillar2.b_core_processing_patterns.b_normalize_records import normalize_users
+
+def test_normalize_user_returns_new_dictionaries():
+    records = [
+    {
+        "name": "  juan bravo  ",
+        "email": "  JUAN.BRAVO@EXAMPLE.COM  ",
+        "country": "  spain  ",
+    },
+    {
+        "name": "mARÍA lópez",
+        "email": " Maria.Lopez@Example.com ",
+        "country": "colombia",
+    },
+    {
+        "name": "  anna kowalska",
+        "email": "ANNA.KOWALSKA@EXAMPLE.COM",
+        "country": "  poland ",
+    },
+    ]
+    normalized = normalize_users(records)
+
+    assert normalized is not records
+    assert all(
+        norm_record is not orig_record 
+        for norm_record, orig_record 
+        in zip(normalized, records)
+    )
+
+def test_normalize_returns_normalized_dicts_in_same_order():
+    records = [
+        {
+            "name": "  juan bravo  ",
+            "email": "  JUAN.BRAVO@EXAMPLE.COM  ",
+            "country": "  spain  ",
+        },
+        {
+            "name": "  anna kowalska",
+            "email": "ANNA.KOWALSKA@EXAMPLE.COM",
+            "country": "  poland ",
+        },
+    ]
+    normalized = normalize_users(records)
+    assert normalized[0] == {
+        "name": "Juan Bravo",
+        "email": "juan.bravo@example.com",
+        "country": "SPAIN",
+    }
+    assert normalized[1] == {
+        "name": "Anna Kowalska",
+        "email": "anna.kowalska@example.com",
+        "country": "POLAND",
+    }
+
